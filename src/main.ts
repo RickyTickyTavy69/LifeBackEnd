c330ee54ad543a2ea6c726913571aad78cbcee6e
artem <bakirovartem69@gmail.com>
2022-12-01 18:21:03 +0100

    delete sessions

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,6 @@ import * as passport from "passport";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(
-      session({
-        secret: 'never give up',      // later put it in the env vars.
-        resave: false,
-        saveUninitialized: false,
-        cookie: {maxAge: 3600000}
-      })
-  )                                       // когда создаётся сессия для пользователя, генерируется куки с информацией для доступа к store
-                // то есть инфа пользователя не послылается на клиент. Только куки с ключом доступа до инфы.
   app.use(passport.initialize());
   app.use(passport.session());
 
