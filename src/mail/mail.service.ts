496a38ba496c6f4a18753c7effac7b88a1f2052c
artem <bakirovartem69@gmail.com>
2022-12-01 18:03:21 +0100

    added lodash

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,8 +6,8 @@ export class MailService {
     constructor(private mailerService: MailerService) {}
 
     async sendVerificationEmail(name, email, code){
-                                              // maybe make a random string like with uuid and use it as a query?
-        const url = `"http://192.168.2.164:5000/auth/confirm?name=${name}"`;       // maybe change to localhost?
+
+        const url = `"http://192.168.2.164:5000/auth/confirm?name=${name}"`;
 
         console.log("sending the email to", name, email);
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {UserDto} from "./dto/user.dto";
 
